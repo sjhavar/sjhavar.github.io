c03f8298fe31d19929670593e8c2f82d7082f6f6
Surbhi Jhavar <surbhi.jhavar@gmail.com>
2019-06-26 09:41:31 -0700

    test

--- contact_form_handler.php
+++ contact_form_handler.php
@@ -1,6 +1,11 @@
 <?php
+// the message
+echo "test";
+$msg = "First line of text\nSecond line of text";
 
- mail("surbhi.jhavar@gmail.com","My subject", 'test');
- echo 'success';
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
 
- ?>
+// send email
+mail("surbhi.jhavar@gmail.com","My subject",$msg);
+?>
